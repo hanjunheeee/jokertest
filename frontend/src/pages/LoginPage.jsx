297dc469d607583681d2d40fd7b5f968723578d2
한준희 <1702o505@naver.com>
2026-05-14 19:12:07 +0900

    react-router-dom 라이브러리 추가

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center px-6">
+      <p className="text-lg">로그인 화면입니다</p>
+    </div>
+  )
+}
